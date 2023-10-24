e55b40f203c4b331d4fcca5a21ace0535a82b2bb
Lars Willighagen <lars.willighagen@gmail.com>
2023-10-24 17:29:37 +0200

    fix: fix small issues

--- scripts/apply-fixes.js
+++ scripts/apply-fixes.js
@@ -4,7 +4,7 @@ const path = require('path')
 const file = path.join(__dirname, '..', 'dist', 'sdd.md')
 
 let content = fs.readFileSync(file, 'utf8')
-content = content.replace(/\[(Instar [IVX])\]\(.+?\)/g, '$1')
+content = content.replace(/\[(Instar [IV]+)\]\(.+?\)/g, '$1')
 content = content.replace(
   /(The ridge bordering the jugae is higher and thicker than that of E\. integriceps\.)\\/,
   '$1 `\\dotfill{} `{=tex}`<span style="float: right;">`{=html}`\\mbox{`{=tex}[*E. maurus*](#taxon-117)`}`{=tex} & `\\mbox{`{=tex}[*E. testudinarius*](#taxon-120)`}`{=tex}`</span>`{=html}\\'
